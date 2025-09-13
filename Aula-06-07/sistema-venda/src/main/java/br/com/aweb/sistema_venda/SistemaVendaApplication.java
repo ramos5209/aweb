30db7de93dfb6248aca920b9bccdb36f8c1c91b6
ramos <viniciusaugusto6996@gmail.com>
2025-09-12 22:54:36 -0300

    docker-compose criado

---
+++ Aula-06-07/sistema-venda/src/main/java/br/com/aweb/sistema_venda/SistemaVendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.aweb.sistema_venda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVendaApplication.class, args);
+	}
+
+}
---
+++ Aula-06-07/sistema-venda/src/test/java/br/com/aweb/sistema_venda/SistemaVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aweb.sistema_venda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
